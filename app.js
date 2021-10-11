3defaa02956c0356d5350ae6a6aafcf5a32301f7
unknown <peterbabalola44@gmail.com>
2021-10-11 14:49:25 +0100

    op2

--- app.js
+++ app.js
@@ -5,12 +5,6 @@ const server = http.createServer((req, res) => {
         res.end('Home Page')
     }
     if (req.url === '/about') {
-        // Blocking Code!!!
-        // for(let i = 0; i<100; i++){
-        //     for(let j = 0; j<100; j++){
-        //         console.log(`${i} ${j}`);
-        //     }
-        // }
         res.end('About Page')
     }
     res.end('Error Page')
