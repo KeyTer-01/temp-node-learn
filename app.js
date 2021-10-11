d3cb200ade10490d5af35ab5e16fbe326ec92726
unknown <peterbabalola44@gmail.com>
2021-10-11 14:48:55 +0100

    op

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const _ = require('lodash')
+const http = require('http')
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Home Page')
+    }
+    if (req.url === '/about') {
+        // Blocking Code!!!
+        // for(let i = 0; i<100; i++){
+        //     for(let j = 0; j<100; j++){
+        //         console.log(`${i} ${j}`);
+        //     }
+        // }
+        res.end('About Page')
+    }
+    res.end('Error Page')
+})
 
-console.log(newItems);
-console.log('Hello World');
+server.listen(5000, () => {
+    console.log('Server listening on port: 5000... ');
+})
