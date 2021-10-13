d7fd9b5c49ca06744e944dea62430c2d9b142d9c
unknown <peterbabalola44@gmail.com>
2021-10-13 18:06:21 +0100

    wednesday

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-const http = require('http')
+const {readFile, writeFile} = require('fs').promises;
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('Home Page')
-    }
-    if (req.url === '/about') {
-        res.end('About Page')
-    }
-    res.end('Error Page')
-})
 
-server.listen(5000, () => {
-    console.log('Server listening on port: 5000... ');
-})
+const start = async() => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/second.txt', 'utf8');
+        await writeFile('./content/result-mind-grenade.txt', `This is Awesome: ${first} - ${second}`)
+    console.log(first, second);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//     if(err){
+//         reject(err);
+//     } else{
+//         resolve(data);
+//     }
+// })
+//     })
+// }
+
+// getText('./content/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err));
